ea96e6ed4bddcc2334a16724acf169372f7501d0
jakabfia <jakabfia@yahoo.com>
2016-09-13 13:06:23 +0200

    second commit

---
+++ src/main/java/de/deutscheboerse/demo/core/MainController.java
@@ -0,0 +1,15 @@
+package de.deutscheboerse.demo.core;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String main() {
+		return "Hello World 2";
+	}
+}
