09eb2ee548163517667f6e7b66a5c0b2e7e9a325
jakabfia <jakabfia@yahoo.com>
2016-09-21 22:15:43 +0200

    switch to cats

--- src/main/java/de/deutscheboerse/demo/core/MainController.java
+++ src/main/java/de/deutscheboerse/demo/core/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
 	@RequestMapping("/")
 	@ResponseBody
 	public String main() {
-		// return "<div> <h1>Here are some cats </h1> </div> <img src='cats.jpg' alt='cats' title='cats'/>";
-		return "<div> <h1>Here are some dogs </h1> </div> <img src='dogs.jpg' alt='dogs' title='dogs'/>";
+		return "<div> <h1>Here are some cats </h1> </div> <img src='cats.jpg' alt='cats' title='cats'/>";
+		// return "<div> <h1>Here are some dogs </h1> </div> <img src='dogs.jpg' alt='dogs' title='dogs'/>";
 	}
 }
